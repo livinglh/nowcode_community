e2493fb5cc09502b69d577c8b66a1883c1d62ec8
livinglh <390866592@qq.com>
2020-05-28 13:43:36 +0800

    项目初始化，首页index显示及换页功能实现

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    // userId可能会传也可能不会，不传的时候就不用管，传了的话就用传的id，sql里会动态生成
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    // 如果需要动态拼一个条件，并且这个方法有且只有这一个条件，这个时候参数前面必须取别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
