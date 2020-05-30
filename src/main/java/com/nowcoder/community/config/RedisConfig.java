80caa08690e071f40239e0586ae6dc53c53c82a6
livinglh <390866592@qq.com>
2020-05-30 09:44:41 +0800

    redis入门及spring整合redis

---
+++ src/main/java/com/nowcoder/community/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // 对原有的RedisTeplate进行覆盖
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){ // Spring会自动注入参数里的factory
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
