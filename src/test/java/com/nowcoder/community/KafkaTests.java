539bc6c7b079754ba6ddf9b109ea72caeb387e42
livinglh <390866592@qq.com>
2020-05-30 16:20:33 +0800

    spring整合kafka

---
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -0,0 +1,55 @@
+package com.nowcoder.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() {
+        // 生产者是主动调用发消息，消费者处理消息是被动的
+        kafkaProducer.sendMessage("test", "你好");
+        kafkaProducer.sendMessage("test", "在吗");
+
+        // 不马上停止，等消费者消费
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"}) // Spring会自动监听test主题，读到之后交给它修饰的方法去处理
+    public void handleMessage(ConsumerRecord record) { // 会把消息自动封装到ConsumerRecord里
+        System.out.println(record.value());
+    }
+
+}
