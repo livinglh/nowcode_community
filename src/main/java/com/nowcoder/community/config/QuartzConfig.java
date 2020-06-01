108528b894e043adc286a741e199fd9e73ea82fe
livinglh <390866592@qq.com>
2020-06-01 11:03:12 +0800

    分布式定时任务：spring quartz

---
+++ src/main/java/com/nowcoder/community/config/QuartzConfig.java
@@ -0,0 +1,45 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.quartz.AlphaJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+// 只有第一次配置有用，配置好了以后，每次都是通过数据库来调用
+@Configuration
+public class QuartzConfig {
+
+    // BeanFactory是容器的顶层接口
+    // 而FactoryBean可简化Bean的实例化过程：相当于这个类里面封装了详细的JobDetail实例化过程
+    // 1.Spring通过FactoryBean封装了Bean的实例化过程
+    // 2.将FactoryBean装配到Spring容器里
+    // 3.将FactoryBean注入给其他的Bean
+    // 4.那么其他的Bean得到的是FactoryBean所管理的对象实例
+
+    // 配置JobDetail
+    @Bean
+    public JobDetailFactoryBean alphaJobDetail(){
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(AlphaJob.class);
+        factoryBean.setName("alphaJob");
+        factoryBean.setGroup("alphaJobGroup");
+        factoryBean.setDurability(true); // 是否持久保存
+        factoryBean.setRequestsRecovery(true); // 是否可恢复
+        return factoryBean;
+    }
+
+    // 配置Trigger（可使用SimpleTriggerFactoryBean或CronTriggerFactoryBean）
+    @Bean
+    public SimpleTriggerFactoryBean alphaTrigger(JobDetail alphaJobDetail){ //当Bean多了之后，会优先注入同名的
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(alphaJobDetail);
+        factoryBean.setName("alphaTrigger");
+        factoryBean.setGroup("alphaTriggerGroup");
+        factoryBean.setRepeatInterval(3000); // 频率
+        factoryBean.setJobDataAsMap(new JobDataMap());
+        return factoryBean;
+    }
+}
